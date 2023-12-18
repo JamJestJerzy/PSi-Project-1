05df682faf53091dc222c26130aef9bb68dcc9e0
Eikooooc <cooookieanonymous.2o@gmail.com>
2023-12-18 13:22:38 +0100

    Added h1 animation

--- scripts/index.js
+++ scripts/index.js
@@ -8,3 +8,49 @@ const handleNav = () => {
 
 
 window.addEventListener('scroll', handleNav)
+
+/* Settings */
+const writeCooldown = 2000;
+const eraseCooldown = 1000;
+
+const typedText = $(".typed-text");
+const cursor = $(".cursor");
+
+const textArray = ["Jerzy", "Oskar"];
+
+let textArrayIndex = 0;
+let charIndex = 0;
+
+const erase = () => {
+    if (charIndex > 0) {
+        cursor.removeClass('blink');
+        //cursor.classList.remove('blink');
+        typedText.text(textArray[textArrayIndex].slice(0, charIndex - 1));
+        charIndex--;
+        setTimeout(erase, 80);
+    } else {
+        cursor.addClass('blink');
+        //cursor.classList.add('blink');
+        textArrayIndex++;
+        if (textArrayIndex > textArray.length - 1) {
+            textArrayIndex = 0;
+        }
+        setTimeout(type, eraseCooldown);
+    }
+}
+
+const type = () => {
+    if (charIndex <= textArray[textArrayIndex].length - 1) {
+        cursor.removeClass('blink');
+        typedText.text(typedText.text() + textArray[textArrayIndex].charAt(charIndex));
+        charIndex++;
+        setTimeout(type, 120);
+    } else {
+        cursor.addClass('blink');
+        setTimeout(erase, writeCooldown);
+    }
+}
+
+$(function (){
+    type();
+}) 
